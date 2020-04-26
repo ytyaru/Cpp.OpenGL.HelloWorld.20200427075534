5b976330625d7525d62ff27f9fcf9c827d656683
ytyaru <yttry0@gmail.com>
2020-04-27 08:40:41 +0900

    update

--- src/run.sh
+++ src/run.sh
@@ -6,6 +6,8 @@ sudo apt install -y libglfw3 libglfw3-dev
 sudo apt install -y libglew-dev
 sudo apt -y install freeglut3 freeglut3-dev
 sudo apt -y install libglu1-mesa-dev mesa-common-dev
+sudo apt -y install libgles2-mesa-dev libegl1-mesa-dev xorg-dev
+sudo apt -y install libglew-dev
 sudo apt -y install libbullet-dev
 g++ -Wall -Weffc++ -o glut_main main.cpp -lglut -lGLU -lGL
 ./glut_main
